1d1da0ef01d3ccc88cd8177e0caa1327946b5ca5
Micha <mich4el.flores@gmail.com>
2022-10-31 15:18:43 -0300

    Fix dataUser.sh path in userList.sh

--- src/scripts/users/usersList.sh
+++ src/scripts/users/usersList.sh
@@ -3,7 +3,7 @@ usersList() {
     echo -e "------------------- Usuarios en el sistema -------------------\n"
     users=$(grep home /etc/passwd | cut -d : -f 1)
     for user in $users; do
-        source ./users/dataUser.sh
+        source /project/2022_Proyecto_Integrador_Equipo_2_Back-end/src/scripts/users/dataUser.sh
         dataUser "$user"
     done
     echo "--------------------------------------------------------------"
